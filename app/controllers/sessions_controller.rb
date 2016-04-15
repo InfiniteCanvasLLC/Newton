fb19ecd0d4670e23e52a33811db82949079e0f45
Rishi Gupta <rishig@mac.com>
2016-04-14 17:58:24 -0700

    Redirect to new account page after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-        puts "request is "
-
         auth = request.env["omniauth.auth"]
         user = User.where(  :provider => auth['provider'], 
                             :uid => auth['uid']).first || User.create_with_omniauth(auth)
+
         session[:user_id] = user.id
+        session[:fb_info] = auth.info
 
-        puts auth['provider']
-        puts auth['uid'].first
-        puts root_url
-
-        redirect_to root_url, :notice => "Signed in!"
+        redirect_to controller: 'new_account'
     end
 
     def destroy
