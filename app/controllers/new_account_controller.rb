86505b1dc48b2e0a96d095212ff6c18856406675
Rishi Gupta <rishig@mac.com>
2016-04-24 01:17:28 -0700

    Pull the current user's actions and display them

--- app/controllers/new_account_controller.rb
+++ app/controllers/new_account_controller.rb
@@ -7,6 +7,24 @@ class NewAccountController < ApplicationController
         user_id = session[:user_id]
         actions = UserAction.where("user_id = " + user_id.to_s)
 
-        byebug
+        @num_actions = actions.count
+
+        actions_array = actions.to_a
+
+        @display_actions = Array.new
+
+        actions_array.each do |cur_action|
+            cur_display_action = Hash.new
+
+            if (cur_action.action_type == 0)
+                cur_display_action["type"] = "Question"
+
+                question = Question.find(cur_action.action_id)
+
+                cur_display_action["text"] = question.text
+            end
+
+            @display_actions << cur_display_action
+        end
     end
 end
