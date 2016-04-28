668cb65b067a7d33a0c719e7350f972aa552f8f3
Rishi Gupta <rishig@mac.com>
2016-04-27 21:46:59 -0700

    Display more question info

--- app/controllers/new_account_controller.rb
+++ app/controllers/new_account_controller.rb
@@ -11,20 +11,13 @@ class NewAccountController < ApplicationController
 
         actions_array = actions.to_a
 
-        @display_actions = Array.new
+        @questions = Array.new
 
         actions_array.each do |cur_action|
-            cur_display_action = Hash.new
 
             if (cur_action.action_type == 0)
-                cur_display_action["type"] = "Question"
-
-                question = Question.find(cur_action.action_id)
-
-                cur_display_action["text"] = question.text
+                @questions << Question.find(cur_action.action_id)
             end
-
-            @display_actions << cur_display_action
         end
     end
 end
