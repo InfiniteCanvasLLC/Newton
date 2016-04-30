9fc8bb4720db80a869d57f96c968e849839ca0f7
Rishi Gupta <rishig@mac.com>
2016-04-30 16:49:34 -0700

    Add scaffold for Parties

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :parties
   resources :user_actions
   resources :questions
   get 'sessions/new'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424032850) do
+ActiveRecord::Schema.define(version: 20160430222519) do
+
+  create_table "parties", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
