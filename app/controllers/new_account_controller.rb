98d0b6253b15a816a6b6bb14425f119e3a40fa4b
Serge <sergemetral@gmail.com>
2016-05-01 12:37:59 -0700

    From the question subtype, lookup the string to display (maybe there is a better way)

--- app/controllers/new_account_controller.rb
+++ app/controllers/new_account_controller.rb
@@ -14,6 +14,7 @@ class NewAccountController < ApplicationController
         actions_array = actions.to_a
 
         @questions = Array.new
+        @question_types = [ "Multiple Choice", "Yes/No", "Rating", "Text" ]
 
         actions_array.each do |cur_action|
 
