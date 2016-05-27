9d631523ae4ed998ed96eb91956ae3c05f513d8b
Hamza Hutchinson <hamza.hutchinson@gmail.com>
2016-05-27 07:20:11 -0700

    Moving the random name function to be a static function on the Party model

--- app/controllers/new_account_controller.rb
+++ app/controllers/new_account_controller.rb
@@ -170,18 +170,13 @@ class NewAccountController < ApplicationController
         # if user.parties is empty we create a default party 0
         if user.parties.count == 0
            default_party = Party.new
-           default_party.name = get_random_party_name()
+           default_party.name = Party.random_name
            default_party.owner_user_id = user.id
            default_party.save
            user.parties << default_party
         end
 
         return user.party_at_index( user.current_party_index.to_i )
-    end
-
-    #this function returns a random party name
-    def get_random_party_name()
-        return  Faker::Superhero.name + "'s " + Faker::Superhero.power
     end
 
 end
