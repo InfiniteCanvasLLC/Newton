6d3f715412283ae273fe5734a9b82f371322a952
Serge <sergemetral@gmail.com>
2016-05-30 22:29:43 -0700

    slightly cleaner

--- app/models/link_to.rb
+++ app/models/link_to.rb
@@ -1,19 +1,40 @@
 class LinkTo < ActiveRecord::Base
     def get_type_id_string
         case self.type_id
-        when 0
+        when LinkTo.standard_type
             return "Standard LinkTo" #Link to youtube, FB, etc...
         #special meaning (ToDos)
-        when 1
+        when LinkTo.sync_spotify_type
             return "Sync Spotify info"
-        when 2
+        when LinkTo.party_invitaion_type
             return "New party invitation"
-        when 3
+        when LinkTo.invite_friend_type
             return "Invite a friend to party"
-        when 4
+        when LinkTo.request_to_join_party_type
             return "Request to join party"
         else
             return "UNKNOW"
         end
     end
+  
+    
+  def self.standard_type
+    return 0
+  end
+  
+  def self.sync_spotify_type
+    return 1
+  end
+  
+  def self.party_invitaion_type
+    return 2
+  end
+  
+  def self.invite_friend_type
+    return 3
+  end
+  
+  def self.request_to_join_party_type
+    return 4
+  end
 end
