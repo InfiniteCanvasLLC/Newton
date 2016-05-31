7645c39d68c289f1a1a494497eea7d6caeb292b5
Serge <sergemetral@gmail.com>
2016-05-30 22:22:12 -0700

    Adding support for special types of linktos. For certain actions we want the user to undertake, we need to be able to assignt specific types of linkto (such as user party request, request for syncing spotify info, etc....)

--- app/models/link_to.rb
+++ app/models/link_to.rb
@@ -1,2 +1,19 @@
 class LinkTo < ActiveRecord::Base
+    def get_type_id_string
+        case self.type_id
+        when 0
+            return "Standard LinkTo" #Link to youtube, FB, etc...
+        #special meaning (ToDos)
+        when 1
+            return "Sync Spotify info"
+        when 2
+            return "New party invitation"
+        when 3
+            return "Invite a friend to party"
+        when 4
+            return "Request to join party"
+        else
+            return "UNKNOW"
+        end
+    end
 end
