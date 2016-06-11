57fc45cf864d8b8e1966789dc5aa48fe3120e66d
Rishi Gupta <rishig@mac.com>
2016-06-11 14:39:18 -0700

    Begin adding some functional tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "get all users" do
+    get :index
+    assert_response :success
+    assert_select '#user_table', 1
+
+    users = User.all
+    index = 0
+
+    assert_select '#user_table > tbody > tr' do |rows|
+
+        index = 0
+
+        rows.each do |cur_row|
+
+            cur_user = users[index]
+
+            cur_cells = cur_row > 'td'
+            assert cur_cells[0].children[0].content == cur_user.name
+            assert cur_cells[1].children[0].content == cur_user.email
+
+            index += 1
+        end
+
+    end
+
+  end
+
+  test "get specific user" do
+    test_user = User.all.first
+
+    get :show, {'id' => test_user.id}
+    assert_response :success
+  end
+
 end
