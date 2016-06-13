685bec0b47a35dc29b0b1f935a90e42b20209740
Serge <sergemetral@gmail.com>
2016-06-12 19:44:34 -0700

    Fix the default birthday date

--- db/migrate/20160605054402_add_columns_to_user.rb
+++ db/migrate/20160605054402_add_columns_to_user.rb
@@ -2,7 +2,7 @@ class AddColumnsToUser < ActiveRecord::Migration
   def change
     add_column :users, :secondary_email, :string, :default => ""
     add_column :users, :gender, :integer, :default => 0
-    add_column :users, :birthday, :date, :default => Time.now
+    add_column :users, :birthday, :date, :default => '2016-06-07'
     add_column :users, :zip_code, :integer, :default => 0
     add_column :users, :description, :text, :default => ""
   end
