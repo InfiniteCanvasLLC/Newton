54164bc33fa58666aaf9bfdb753ef1bc54a2dffc
Rishi Gupta <rishig@mac.com>
2016-06-27 00:55:42 -0700

    Lock this down and fix tests

--- test/controllers/user_feedbacks_controller_test.rb
+++ test/controllers/user_feedbacks_controller_test.rb
@@ -3,8 +3,15 @@ require 'test_helper'
 class UserFeedbacksControllerTest < ActionController::TestCase
 
   test "load_page" do
+    session[:user_id] = users(:steve_jobs).id
     get :index
     assert_response :success
+  end
+
+test "load_page non_admin" do
+    session[:user_id] = users(:steve_wozniak).id
+    get :index
+    assert_response(500)
   end
 
 end
