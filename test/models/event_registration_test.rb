27c50941de465b702150b697043afa84f6a75a5b
Rishi Gupta <rishig@mac.com>
2016-07-28 02:15:42 -0700

    Last of event registration validations

--- test/models/event_registration_test.rb
+++ test/models/event_registration_test.rb
@@ -6,13 +6,19 @@ class EventRegistrationTest < ActiveSupport::TestCase
   # end
 
   test "Event and party validation" do
-    event = EventRegistration.create(:party_id => 1)
+    event = EventRegistration.create(:party_id => 1, :user_id => 1, :event_id => 1)
     assert_not(event.valid?)
 
     event = EventRegistration.create(:party_id => parties(:hello_kitty_party).id)
     assert_not(event.valid?)
 
     event = EventRegistration.create(:party_id => parties(:hello_kitty_party).id, :user_id => users(:steve_wozniak).id)
+    assert_not(event.valid?)
+
+    event = EventRegistration.create(:party_id => parties(:hello_kitty_party).id, :user_id => users(:steve_wozniak).id, :event_id => events(:one).id, :commitment => 4)
+    assert_not(event.valid?)
+
+    event = EventRegistration.create(:party_id => parties(:hello_kitty_party).id, :user_id => users(:steve_wozniak).id, :event_id => events(:one).id, :commitment => EventRegistration.user_going)
     assert(event.valid?)
 
   end
