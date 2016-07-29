4fd28063b3ec1f78bd4795ede8c81ad315309396
Rishi Gupta <rishig@mac.com>
2016-07-29 00:11:45 -0700

    Add event tests

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ActiveRecord::Base
     has_and_belongs_to_many :parties
     validate :valid_event_type
+    validate :valid_parties
+    validates_datetime :start
 
     # We don't have any event types, so this should always be nil for now.  Otherwise we might introduce garbage values that have the wrong meaning later
     def valid_event_type
@@ -8,4 +10,14 @@ class Event < ActiveRecord::Base
             errors.add(:event_type, "Event type must be nil")
         end
     end
+
+    def valid_parties
+
+        self.parties.each do |cur_party|
+            if (!Party.exists?(cur_party.id))
+                errors.add(:invalid_party, "Attempting to add an invalid party")
+            end
+        end
+    end
+
 end
