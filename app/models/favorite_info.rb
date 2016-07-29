587e57b23c7bc9f1bb518006803c041350f7fc4b
Hamza Hutchinson <hamza.hutchinson@gmail.com>
2016-07-28 19:55:04 -0700

    Cleaning up the top artists and top songs display on the admin page

--- app/models/favorite_info.rb
+++ app/models/favorite_info.rb
@@ -1,3 +1,25 @@
 class FavoriteInfo < ActiveRecord::Base
   belongs_to :user
+
+  def top_artists_to_genres
+    hash = Hash.new
+    if !top_artists.nil?
+      parsed = JSON.parse(top_artists)
+      parsed.count.times do |i|
+        hash[parsed[i.to_s]["name"]] = parsed[i.to_s]["genres"]
+      end
+    end
+    return hash
+  end
+
+  def top_songs_to_artists
+    hash = Hash.new
+    if !top_artists.nil?
+      parsed = JSON.parse(top_songs)
+      parsed.count.times do |i|
+        hash[parsed[i.to_s]["name"]] = parsed[i.to_s]["artist"]
+      end
+    end
+    return hash
+  end
 end
