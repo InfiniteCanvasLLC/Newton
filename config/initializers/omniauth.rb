05bd6e18807cb564869afff501bd9b1acfc882a0
Rishi Gupta <rishig@mac.com>
2016-09-15 00:40:45 -0700

    Add Mixpanel

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,6 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   puts "Spotify ID: " + ENV['SPOTIFY_ID']
   puts "Spotify Secret: " + ENV['SPOTIFY_SECRET']
   puts "Google Analytics ID: " + ENV['GOOGLE_ANALYTICS_ID']
+  puts "Mixpanel ID: " + ENV['MIXPANEL_ID']
   
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            :scope => 'email,user_birthday,public_profile,user_location', :display => 'page', :secure_image_url => true
