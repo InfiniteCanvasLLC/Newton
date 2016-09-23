4f20fe8a5212109b558e370cf0d0cf58a87188a3
Serge <sergemetral@gmail.com>
2016-09-23 11:58:39 -0700

    Allow production to access public assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Gmail email delivery setup
+  config.action_mailer.asset_host = "audicy.us"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
