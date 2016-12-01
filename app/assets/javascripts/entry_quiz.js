a93d32032aab3d913b4c0feeab8d336c6da42628
Rishi Gupta <rishig@mac.com>
2016-12-01 02:12:20 -0800

    Populate quiz question

--- app/assets/javascripts/entry_quiz.js
+++ app/assets/javascripts/entry_quiz.js
@@ -15,7 +15,13 @@ function EntryQuizViewModel()
     var self = this;
 
     self.questionText = ko.observable(questions[cur_question].text);
+    self.questionReplies = ko.observableArray(questions[cur_question].replies);
+
     self.showIntro = ko.observable(true);
+
+    self.getStarted = function() {
+        self.showIntro(false);
+    }
 }
 
 ko.applyBindings(new EntryQuizViewModel());
