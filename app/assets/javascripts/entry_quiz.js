0212c31025475de59590ec4ab34994080931eccf
Rishi Gupta <rishig@mac.com>
2016-12-04 01:49:09 -0800

    Hook up logic for storing question answers, and add second question

--- app/assets/javascripts/entry_quiz.js
+++ app/assets/javascripts/entry_quiz.js
@@ -5,9 +5,19 @@ var questions = [
     {
         text: "What do you feel was the best era of music?",
         replies: [ "2000s", "1990s", "1980s", "1970s or earlier"]
+    },
+// Second question
+    {
+        text: "Where do you listen to most of your music?",
+        replies: [ "While travelling (eg: car, plane, train, etc)", "At work", "At the club", "At the gym"]
     }
 ]
 
+var answers = []
+
+var INVALID_INDEX = -1;
+
+var answer_index = INVALID_INDEX;
 var cur_question = 0;
 
 function EntryQuizViewModel()
@@ -23,11 +33,21 @@ function EntryQuizViewModel()
         self.showIntro(false);
     }
 
-    self.radioButtonClicked = function() {
+    self.radioButtonClicked = function(radioButton, event) {
         $("#next-button").addClass("active");
         $("#next-button").removeClass("disabled");
 
+        answer_index = $(event.target).attr("answerindex");
+
         return true;
+    }
+
+    self.nextButtonClicked = function() {
+        answers[cur_question] = parseInt(answer_index);
+        cur_question++;
+
+        self.questionText(questions[cur_question].text);
+        self.questionReplies(questions[cur_question].replies);
     }
 }
 
